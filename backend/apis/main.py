911e46bb786aa499da4157004005a14aab239384
nicotsessilede <nicotsessilede12@gmail.com>
2022-01-15 03:32:47 +0100

    subscription api first

---
+++ backend/apis/main.py
@@ -0,0 +1,41 @@
+import email
+from fastapi import FastAPI, status
+
+from pydantic import BaseModel
+
+from typing import list, Optional
+
+app = FastAPI()
+
+class Inscription(BaseModel):
+    name : str
+    email: str
+    passord: str
+    genre: str
+    statut: str
+    photo: Optional[str]
+    tel: int
+    
+@app.get("/")
+def root():
+    return "subscription"
+
+@app.post("/subscription ", status_code=status.HTTP_201_CREATED)
+def create_subscription(inscription: Inscription ):
+    return "create subscription "
+
+@app.get("/subscription/{id}")
+def read_subscription(id: int):
+    return "read subscription  with id {id}"
+
+@app.put("/subscription/{id}")
+def update_subscription(id: int):
+    return "update subscription  with id {id}"
+
+@app.delete("/subscription/{id}")
+def delete_subscription(id: int):
+    return "delete subscription  with id {id}"
+
+@app.get("/subscription")
+def read_subscription_list():
+    return "read subscription list"
