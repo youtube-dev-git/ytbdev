e3f37abf261e02d7aee53c0c7be92de409d0c7f0
Ico no Yuri <iconoyuri@gmail.com>
2022-01-19 07:48:33 +0100

    Merge branch 'main' of https://github.com/youtube-dev-git/ytbdev

--- backend/DAO/DAOObjects.py
+++ backend/DAO/DAOObjects.py
@@ -1,17 +1,21 @@
 from typing import List
-from ..Permanent_Classes.syllabus import Syllabus
-from ..Permanent_Classes.users import User, Learner, Admin, Expert
+from Permanent_Classes.syllabus import Syllabus
+from Permanent_Classes.users import User, Learner, Admin, Expert
+
+
 class DAOObject:
     def __init__(self) -> None:
         pass
     
     @classmethod
-    def save_syllabus(self, expert_id: int, syllabus : Syllabus) -> None :
-        # La fonction save_syllabus renvoie l'identifiant en BD du syllabus qui a été stocké
+    def save_syllabus(self,expert_id: int,syllabus : Syllabus) -> int :
+        # Cette fonction renvoie l'identifiant en BD du syllabus qui a été stocké
         pass
     
     @classmethod
-    def save_syllabus_videos(self, syllabus : Syllabus) -> int :
+    def save_syllabus_videos(self, syllabus : Syllabus, syllabus_id: int) -> None :
+        # Cette fonction prend en paramètre un syllabus muni de ses vidéos et actualise
+        # de fait le syllabus d'id syllabus_id
         pass
     
     @classmethod
