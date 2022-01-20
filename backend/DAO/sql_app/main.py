2ad28cdd8210bc4d0670b4737eff41948742589b
nicotsessilede <nicotsessilede12@gmail.com>
2022-01-20 17:12:14 +0100

    experrt, learner, admin correct

--- backend/DAO/sql_app/main.py
+++ backend/DAO/sql_app/main.py
@@ -1,3 +1,4 @@
+
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
@@ -16,7 +17,7 @@ def get_db():
         yield db
     finally:
         db.close()
-# get user
+# get admin
 
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
@@ -39,47 +40,71 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
-#get admin
+######################
 
-@app.post("/admins/", response_model=schemas.User)
-
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return crud.create_user(db=db, user=user)
-
-
+@app.post("/admins/", response_model=schemas.Admin)
 def create_admin(admin: schemas.AdminCreate, db: Session = Depends(get_db)):
-    db_admin = crud.get_admin_by_email(db, email=admin.users.email)
+    db_admin = crud.get_admin_by_email(db, email=admin.email)
     if db_admin:
         raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_admin(db=db, admin=admin)
 
 
-@app.get("/admins/", response_model=list[schemas.User])
+@app.get("/admins/", response_model=list[schemas.Admin])
 def read_admins(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     admins = crud.get_admins(db, skip=skip, limit=limit)
     return admins
 
 
-@app.get("/admins/{admin_id}", response_model=schemas.User)
+@app.get("/admins/{admin_id}", response_model=schemas.Admin)
 def read_admin(admin_id: int, db: Session = Depends(get_db)):
     db_admin = crud.get_admin(db, admin_id=admin_id)
     if db_admin is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=404, detail="Admin not found")
     return db_admin
 
+#######################
+
+@app.post("/experts/", response_model=schemas.Expert)
+def create_expert(expert: schemas.ExpertCreate, db: Session = Depends(get_db)):
+    db_expert = crud.get_expert_by_email(db, email=expert.email)
+    if db_expert:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_expert(db=db, expert=expert)
 
 
-@app.post("/admins/{admin_id}/items/", response_model=schemas.Item)
-def create_item_for_admin(
-    admin_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
-):
-    return crud.create_admin_item(db=db, item=item, admin_id=admin_id)
+@app.get("/experts/", response_model=list[schemas.Expert])
+def read_experts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    experts = crud.get_experts(db, skip=skip, limit=limit)
+    return experts
 
 
-@app.get("/items/", response_model=list[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
-    return items
+@app.get("/experts/{expert_id}", response_model=schemas.Expert)
+def read_expert(expert_id: int, db: Session = Depends(get_db)):
+    db_expert = crud.get_expert(db, expert_id=expert_id)
+    if db_expert is None:
+        raise HTTPException(status_code=404, detail="Expert not found")
+    return db_expert
+
+#########################
+
+@app.post("/learners/", response_model=schemas.Learner)
+def create_learner(learner: schemas.LearnerCreate, db: Session = Depends(get_db)):
+    db_learner = crud.get_learner_by_email(db, email=learner.email)
+    if db_learner:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_learner(db=db, learner=learner)
+
+
+@app.get("/learners/", response_model=list[schemas.Learner])
+def read_learners(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    learners = crud.get_learners(db, skip=skip, limit=limit)
+    return learners
+
+
+@app.get("/learners/{learner_id}", response_model=schemas.Learner)
+def read_learner(learner_id: int, db: Session = Depends(get_db)):
+    db_learner = crud.get_learner(db, learner_id=learner_id)
+    if db_learner is None:
+        raise HTTPException(status_code=404, detail="Learner not found")
+    return db_learner
