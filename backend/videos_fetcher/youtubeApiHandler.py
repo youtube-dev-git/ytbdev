da3a574033b54d16f49602aac9bad21f8b3751f4
Ico no Yuri <iconoyuri@gmail.com>
2022-01-20 19:10:59 +0100

    Correction des bugs de requêtage

--- backend/videos_fetcher/youtubeApiHandler.py
+++ backend/videos_fetcher/youtubeApiHandler.py
@@ -1,32 +1,14 @@
-# from Permanent_Classes.lesson import Lesson
 from googleapiclient.discovery import build
 
-# import os
-
-# import google_auth_oauthlib.flow
-# import googleapiclient.discovery
-# import googleapiclient.errors
 
 class YoutubeAPIHandler :
     API_NAME = "youtube"
     API_VERSION = "v3"
     API_KEY = "AIzaSyCczWd9sOMfJOKEcbYhneIS_UzrY4dOGuo"
     youtube = build(API_NAME, API_VERSION, developerKey=API_KEY)
-    
-    # scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-    # api_service_name = "youtube"
-    # api_version = "v3"
-    # client_secrets_file = "code_secret_client_581014431912-9f5dn2495d01m9ir7c4d05mic3eceqnb.apps.googleusercontent.com.json"
-    # flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-    #     client_secrets_file, scopes)
-    # credentials = flow.run_console()
-    # youtube = googleapiclient.discovery.build(
-    #     api_service_name, api_version, credentials=credentials)
-
     def __init__(self) -> None:
         pass
     
-    # from Permanent_Classes.syllabus import Syllabus
     @classmethod
     def fetch_by_query(self, syllabus ):
         for lesson in syllabus.lessons:
@@ -39,7 +21,8 @@ class YoutubeAPIHandler :
         request = self.youtube.search().list(
             part='snippet',
             maxResults = 25,
-            q = query
+            q = query,
+            type="video"
         )
         response = request.execute()
         return response
@@ -52,7 +35,7 @@ class YoutubeAPIHandler :
             fields = "items(statistics(viewCount))"
         )
         response = request.execute()
-        return int(response["items"]["statistics"]["viewCount"])
+        return int(response["items"][0]["statistics"]["viewCount"])
     
     @classmethod
     def _format_query(self, video : dict) -> dict:
@@ -60,7 +43,7 @@ class YoutubeAPIHandler :
         return {
             "video_id" : video["id"]["videoId"],
             "title" : video["snippet"]["title"],
-            "viewCount" : YoutubeAPIHandler._get_view_count(video["items"][0]["id"]),
+            "viewCount" : YoutubeAPIHandler._get_view_count(video["id"]["videoId"]),
             "description" : video["snippet"]["description"],
             "published_at" : video["snippet"]["publishedAt"],
             "thumbnails_medium" : video["snippet"]["thumbnails"]["medium"]["url"],
