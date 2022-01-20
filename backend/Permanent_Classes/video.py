d1f836cbdd4264fc5e437b2dfc6c445f665105f1
Ico no Yuri <iconoyuri@gmail.com>
2022-01-20 15:43:53 +0100

    modification du champ videoId en video_id

--- backend/Permanent_Classes/video.py
+++ backend/Permanent_Classes/video.py
@@ -4,13 +4,15 @@ from pydantic.main import BaseModel
 
 
 class Video(BaseModel):
-    videoId : str 
+    
+    video_id : str 
     title : str
     viewCount: int
     description : str 
     published_at : str 
     thumbnails_medium : str
-    channel_id : str 
+    channel_id : str
+    
     def __init__(__pydantic_self__, **data: Any) -> None:
         super().__init__(**data)
 
